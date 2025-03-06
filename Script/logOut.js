1701071f87d4b89133371cea252dd640f9aa7973
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2025-03-07 00:15:21 +0600

    transaction history added

--- Script/logOut.js
+++ Script/logOut.js
@@ -1,3 +1,4 @@
 document.getElementById('log-out-btn').addEventListener('click', function(){
+    event.preventDefault();
     window.location.href = './index.html';
 })
--- Script/transaction.js
+++ Script/transaction.js
@@ -1,4 +1,5 @@
 document.getElementById('transaction-history-box').addEventListener('click', function(){
+    event.preventDefault();
     handleToggle('add-money-section', "none");
     handleToggle('cash-out-section', "none");
     handleToggle('transaction-history-section', "block");
